d43cb9e5d0d6d9202429cab46a17753217bcaaee
mrios5 <mrios5@ucol.mx>
2022-11-24 00:48:57 -0600

    nice

--- libreriasAruco/magicCode.js
+++ libreriasAruco/magicCode.js
@@ -1,20 +1,27 @@
 var video, canvas, context, imageData, detector;
 
 
-const student = [
-    {"id": 0, "name": "foo"},
-    {"id": 1, "name": "Manuel"},
-    {"id": 2, "name": "Gerardo"},
-    {"id": 3, "name": "Angel"},
-    {"id": 4, "name": "Luis"},
-    {"id": 5, "name": "Skittles"},
-    {"id": 6, "name": "Jeff"}
+const students = [
+  {"id": 0, "name": "Test"},
+  {"id": 1, "name": "Cervantes Gonzalez"},
+  {"id": 2, "name": "Chavez Ramirez"},
+  {"id": 3, "name": "Flores Ramirez"},
+  {"id": 4, "name": "Garcia Gonzalez"},
+  {"id": 5, "name": "Montero Valdez"},
+  {"id": 6, "name": "Pastor Martinez"},
+  {"id": 7, "name": "Rios Martinez"},
+  {"id": 8, "name": "Parra Lopez"},
+  {"id": 9, "name": "Guzman Diaz"},
+  {"id": 10, "name": "Contreras Cardenas"},
+  {"id": 11, "name": "Iglesias Jimenez"}
 ]
 
+//funcion para que inicie justo cuando carga la pantalla
 const  onLoad = () => {
+  //toma los elemtos del dom, tanto el video como el cambas mediante id
   video = document.getElementById("video");
   canvas = document.getElementById("canvas");
-  context = canvas.getContext("2d");
+  context = canvas.getContext("2d", { willReadFrequently: true });
 
   canvas.width = parseInt(canvas.style.width);
   canvas.height = parseInt(canvas.style.height);
@@ -62,10 +69,12 @@ const tick = () => {
   if (video.readyState === video.HAVE_ENOUGH_DATA){
     snapshot();
 
+    //Esta variable es la que cuenta con un objeto Ar.marker el cual tiene id y corners (4 como array)
     var markers = detector.detect(imageData);
     drawCorners(markers);
     drawId(markers);
-
+    getId(markers)
+    //console.log(markers)
     //send_data(markers)
   }
 }
@@ -101,12 +110,14 @@ const  drawCorners = (markers) => {
   }
 }
 
+
+//Esta funcion en este caso imprime el nommbre del usuario con su id asi como tambien con coordenadas ubica el nombre de una esquina
 const  drawId = (markers) =>  {
   var corners, corner, x, y, i, j;
   
-  context.strokeStyle = "lightgreen";
-  context.font = 'bold 48px serif'
-  context.lineWidth = 5;
+  context.strokeStyle = "black";
+  context.font = '25px Arial';
+  context.lineWidth = 2;
   
   for (i = 0; i !== markers.length; ++ i){
     corners = markers[i].corners;
@@ -121,40 +132,62 @@ const  drawId = (markers) =>  {
       y = Math.min(y, corner.y);
     }
 
-    //context.strokeText(`Got it ${markers[i].id}`, x, y)
-    context.strokeText(`Got it ${student[markers[i].id].name}`, x, y)
+    context.strokeText(`${students[markers[i].id].name}`, x, y)
+    
+    //LaLista(students[markers[i].id].name)
   }
 }
 
-const send_data = (markers) => {
-    const mongoose = require('mongoose');
-    mongoose.connect('mongodb+srv://admin:admin@cluster0.tzrgkus.mongodb.net/TakeTheList');
-
-    const listData = mongoose.model('people', 
-        { 
-            name: String, 
-            assist: String
-        });
-    
-    const get_data = (name) => {
-        const Data = {"name": `${name}`, "assist": "Yes"}
-        return Data
-    }
-
-    const save_Data = () => {
-        var GoodData = new listData(get_data())
-        GoodData.save()
-        console.log("maybe its done")
-    }
-    
-    var corners, i;
-    
-    for (i = 0; i !== markers.length; ++ i){
-      corners = markers[i].corners;
-      console.log(markers[i].id)
-      get_data(student[markers[i].id].name)
-    }
+// Asi es como se consigue el id de un marker
+const getId = (markers) => {
+  for (let i = 0; i !== markers.length; ++ i){
+    console.log(LaLista(lista, students[markers[i].id].name))
+    //console.log(markers[i].id)
+  }
 }
 
+var lista = []
+
+const takelist = () => {
+  lista = []
+  console.log('Lista limpiada preparada para llenarse :)')
+}
+
+const LaLista = (lista, student) => {
+  if(lista.indexOf(student) === -1 ){
+    lista.push(student)
+  }else {
+    return lista
+  }
+}
+
+const showlist = () =>{
+  console.log('esta es la lista')
+  var thelist  = lista
+  for(let i = 0; i < thelist.length; i++){
+    console.log(thelist[i])
+    document.getElementById("demo").innerHTML = thelist[i]
+    document.createElement("br")
+  }
+
+
+  lista.map((item) => {
+    
+  })
+
+
+}
+
+
+function loadTableData() {
+  const table = document.getElementById("testBody");
+  lista.forEach( item => {
+    let row = table.insertRow();
+    let date = row.insertCell(0);
+    date.innerHTML = item;
+    let name = row.insertCell(1);
+    name.innerHTML = 'yes';
+  });
+}
 
 window.onload = onLoad;
